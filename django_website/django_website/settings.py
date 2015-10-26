d2abcac3225fa6d34e0f04c7c442bf036a6f3377
Shawn Furyan <shawn.p.r@gmail.com>
2015-10-26 13:26:59 -0500

    Removed auto-gen SECRET_KEY

--- django_website/django_website/settings.py
+++ django_website/django_website/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@3s!=%w#tr#fvd)7^914z_%0qllo3v_yw0zr-jotf^ti6^$07#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
