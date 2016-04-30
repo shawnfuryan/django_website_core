903aa419507b82afe910a138026f2b6c5ef07b8c
Shawn Furyan <shawn@furyan.co>
2016-04-30 08:05:22 -0500

    Add internal links to Site header

--- django_website/django_website/urls.py
+++ django_website/django_website/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'blog/', include('blog.urls')),
+    url(r'blog/', include('blog.urls', namespace='blog')),
     url(r'', include('website_core.urls')),
 ]
